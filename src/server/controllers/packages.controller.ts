c56d5fcc61086e6fb30d30ffc32518799e843e0e
IO89 <osipov.ivan.89@gmail.com>
2019-01-05 19:32:51 +0200

    Regex expression added

--- src/server/controllers/packages.controller.ts
+++ src/server/controllers/packages.controller.ts
@@ -6,16 +6,13 @@ const file:string = fs.readFileSync('../../../data/status.real','utf-8');
 
 // Get rid of empty spaces and dots and space breaks
 const removeEmptyValue:string = R.reject(n => n== '');
-const removeSpaceDot:string = R.reject(n => n== ' .');
-const removeNewlineBreak:string = R.split('\n');
-//sort alphabetically
-const alphabeticalOrder = R.sortBy(R.compose(R.toLower, R.prop('name')));
+const removeUndefinedValue:string = R.reject(n => n== undefined);
+
 // Remove special characters from depends field
 const removeSpecialCharactersFromDepends =  R.replace(/[^\w\s]/gi, '');
 
-
 //find single package
-const singePackage = R.compose(
+const singlePackageObj = R.compose(
     R.fromPairs(),
     R.of(),
     R.slice(1,3),
@@ -24,24 +21,38 @@ const singePackage = R.compose(
 //Show package details
 const packageDetail = R.compose(
     R.mergeAll,
-    R.map(singePackage),
+    R.map(singlePackageObj),
     removeEmptyValue,
     R.split('\n'),
     removeSpecialCharactersFromDepends,
+
+);
+//show all packages
+const listAllPackages =
+    R.compose(
+        removeUndefinedValue,
+        R.pluck('Package'),
+        R.map(packageDetail),
+        removeEmptyValue,
+        R.split('\n'),
+    );
+
+const listAllPackagesSorted = listAllPackages(file).sort();
+
+const findPackage= R.find(
+    R.propEq('Package','Name of package')(file)
 );
 
 
-// list packages by prop Package
-const listAllPackages = R.map(R.prop('Package'));
-
-
-
-//console.log('list packages',listAllPackages(file2));
+//console.log('list packages',listAllPackages(packageDetail(file)));
 //console.log('parsePackages', packagesList(file));
 //console.log('file1', file);
 //console.log('file2',file2);
 //console.log('signle package',singePackage(file));
 //console.log('tokeyValuePairs',toKeyValuePairList(file));
 //console.log('segments',toSegments(file));
-console.log('packageDetail',packageDetail(file));
+//console.log('packageDetail',packageDetail(file));
 //console.log('clean',removeSpecialCharactersFromDepends(file));
+//console.log('noDependecies',hasNoDependencies(file));
+//console.log('listallPackages',listAllPackages(file));
+console.log('list all sorted packages',listAllPackagesSorted);
