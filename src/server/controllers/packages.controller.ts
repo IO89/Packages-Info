3568a8a0386d05af3639d7129cea29195d550b43
IO89 <osipov.ivan.89@gmail.com>
2019-01-06 12:44:00 +0200

    Added types

--- src/server/controllers/packages.controller.ts
+++ src/server/controllers/packages.controller.ts
@@ -5,21 +5,21 @@ import R from 'ramda';
 const file:string = fs.readFileSync('../../../data/status.real','utf-8');
 
 // Get rid of empty spaces and dots and space breaks
-const removeEmptyValue:string = R.reject(n => n== '');
-const removeUndefinedValue:string = R.reject(n => n== undefined);
+const removeEmptyValue:(file:string)=> string = R.reject(n => n== '');
+const removeUndefinedValue:(file:string)=> string = R.reject(n => n== undefined);
 
 // Remove special characters from depends field
-const removeSpecialCharactersFromDepends =  R.replace(/[^\w\s]/gi, '');
+const removeSpecialCharactersFromDepends:(file:string)=> string = R.replace(/[^\w\s]/gi, '');
 
 //find single package
-const singlePackageObj = R.compose(
+const singlePackageObj:(file:string) => Array<string> = R.compose(
     R.fromPairs(),
     R.of(),
     R.slice(1,3),
     R.match(/(.*?)\s(.*)/)
 );
 //Show package details
-const packageDetail = R.compose(
+const packageDetail:(file:string) => Array<string> = R.compose(
     R.mergeAll,
     R.map(singlePackageObj),
     removeEmptyValue,
@@ -28,8 +28,7 @@ const packageDetail = R.compose(
 
 );
 //show all packages
-const listAllPackages =
-    R.compose(
+const listAllPackages:(file:string)=> Array<string> = R.compose(
         removeUndefinedValue,
         R.pluck('Package'),
         R.map(packageDetail),
@@ -37,7 +36,7 @@ const listAllPackages =
         R.split('\n'),
     );
 
-const listAllPackagesSorted = listAllPackages(file).sort();
+const listAllPackagesSorted:Array<string> = listAllPackages(file).sort();
 
 const findPackage= R.find(
     R.propEq('Package','Name of package')(file)
@@ -48,11 +47,10 @@ const findPackage= R.find(
 //console.log('parsePackages', packagesList(file));
 //console.log('file1', file);
 //console.log('file2',file2);
-//console.log('signle package',singePackage(file));
-//console.log('tokeyValuePairs',toKeyValuePairList(file));
-//console.log('segments',toSegments(file));
+//console.log('signle package',singlePackageObj(file));
+
 //console.log('packageDetail',packageDetail(file));
 //console.log('clean',removeSpecialCharactersFromDepends(file));
 //console.log('noDependecies',hasNoDependencies(file));
 //console.log('listallPackages',listAllPackages(file));
-console.log('list all sorted packages',listAllPackagesSorted);
+//console.log('list all sorted packages',listAllPackagesSorted);
