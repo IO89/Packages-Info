99236dc5ae6496f88d660169948bfca086592ba6
IO89 <osipov.ivan.89@gmail.com>
2019-01-10 18:56:06 +0200

    shows packages name and descriptions

--- src/server/controllers/packages.controller.ts
+++ src/server/controllers/packages.controller.ts
@@ -11,7 +11,7 @@ const removeUndefinedValue:(file:string)=> string = R.reject(n => n== undefined)
 const removeDotAndSpaceValue:(file:string)=>string = R.reject(n => n==' .');
 
 // Remove special characters from depends field
-const removeSpecialCharactersFromDepends:(file:string)=> string = R.replace(/[^\w\s]/gi, '');
+const removeSpecialCharactersFromDepends:(file:string)=> string = R.replace(/[^\w\s]/gi, ' ');
 
 //find single package:name value pair
 const packageKeyPair:(file:string) => Object = R.compose(
@@ -19,26 +19,53 @@ const packageKeyPair:(file:string) => Object = R.compose(
     R.of(),
     R.slice(1,3),
     R.match(/(.*?)\s(.*)/)
+
 );
-//Show package details
+//Show package details Note:
 const singlePackageObj:(file:string) => Object = R.compose(
     R.mergeAll,
     R.map(packageKeyPair),
     removeEmptyValue,
     R.split('\n'),
-    removeSpecialCharactersFromDepends,
-
+    removeSpecialCharactersFromDepends
 );
+
 //show all packages names
-const listAllPackages:(file:string)=> Array<string> = R.compose(
+const listAllPackagesNames:(file:string)=> Array<string> = R.compose(
         removeUndefinedValue,
         R.pluck('Package'),
         R.map(singlePackageObj),
-        removeEmptyValue,
         R.split('\n'),
     );
 // sort alphabetically
-export const listAllPackagesSorted:Array<string> = listAllPackages(file).sort();
+export const listAllPackagesSorted:Array<string> = listAllPackagesNames(file).sort();
+
+// gather description in one file
+const removeSpaces:(accumulator:Array<string>,element:string) => Array<string> = (accumulator, element) => {
+    element.match(/^\s+/g) ? accumulator[accumulator.length-1] += element : accumulator.push(element);
+    return accumulator
+};
+
+const fullPackagesObj:(file:string)=> Array<string> = R.compose(
+    R.map(singlePackageObj),
+    R.reduce(removeSpaces,[]),
+    R.split('\n'),
+);
+
+const showDescription:(file:string) => Array<string> = R.compose(
+    R.reject(R.isNil),
+    R.pluck('Description'),
+    fullPackagesObj
+);
+//console.log('showDescription',showDescription(file))
+
+const combinedNamesDescriptions = () => {
+  const Array1 = listAllPackagesNames(file);
+  const Array2 = showDescription(file);
+  const combinedArray = R.zip(Array1,Array2);
+  const toPairs = R.fromPairs(combinedArray);
+  //console.log(toPairs);
+};
 
 const searchByName:(name:string) => Object = R.compose(
     R.find(R.propEq('Package', 'makedev')),
@@ -48,25 +75,20 @@ const searchByName:(name:string) => Object = R.compose(
     R.split('\n'),
 );
 
-const showDescriptionFiled:(file:string) => Array<string> = R.compose(
-    removeUndefinedValue,
-    R.pluck('Description'),
-    R.map(singlePackageObj),
-    removeEmptyValue,
-    R.split('\n'),
-);
+//console.log('packageKeyPair',packageKeyPair(file));
+//console.log('packageDetail',singlePackageObj(file));
+//console.log('listallPackages',listAllPackagesNames(file));
+//console.log('fullPackages',fullPackages(file));
 
-
-//console.log('list packages',listAllPackages(packageDetail(file)));
 //console.log('parsePackages', packagesList(file));
-//console.log('file1', file);
+
 
 //console.log('signle package',packageKeyPair(file));
 
-//console.log('packageDetail',singlePackageObj(file));
+
 //console.log('clean',removeSpecialCharactersFromDepends(file));
 //console.log('noDependecies',hasNoDependencies(file));
-//console.log('listallPackages',listAllPackages(file));
+
 //console.log('list all sorted packages',listAllPackagesSorted);
 
 //console.log('findPackage makedev',searchByName(file));
