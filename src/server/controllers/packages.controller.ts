500719d506b3a54dc08e04eedd95590c0539c6f4
IO89 <osipov.ivan.89@gmail.com>
2019-01-11 15:58:41 +0200

    shows packages description

--- src/server/controllers/packages.controller.ts
+++ src/server/controllers/packages.controller.ts
@@ -8,7 +8,9 @@ const file:string = fs.readFileSync(statusReal,'utf-8');
 
 // move element if it starts with space to one index back
 const removeSpaces:(accumulator:Array<string>,element:string) => Array<string> = (accumulator, element) => {
-    element.match(/^\s+/g) ? accumulator[accumulator.length-1] += element : accumulator.push(element);
+    element.match(/^\s+/g)
+        ? accumulator[accumulator.length-1] += element
+        : accumulator.push(element);
     return accumulator
 };
 // Transform array to contain fields with description
@@ -20,10 +22,43 @@ const toFullArray:(file:string)=> Array<string> = R.compose(
 //console.log('fullPackagesObj',fullPackagesObj(file));
 const packageArray:Array<string> = toFullArray(file);
 
-// Separate elements by colon
+// Separate elements by colon and create key value pairs
 const toKeyValuePairs = packageArray.map((element,index)=>{
-    let obj = {};
-    let split = packageArray[index].split(':');
-    obj[split[0].trim()]=split[1];
-    return obj
+    let toObjects = {};
+    const split = packageArray[index].split(':');
+    toObjects[split[0].trim()]=split[1];
+    return toObjects
 });
+//console.log('toKeyValuePairs',toKeyValuePairs);
+
+const showPackagesNames:(file:Object) => Array<string> = R.compose(
+    R.reject(R.isNil),
+    R.pluck('Package')
+);
+//console.log('showPackagesNames',showPackagesNames(toKeyValuePairs));
+
+export const listAllPackagesSorted:Array<string> = showPackagesNames(toKeyValuePairs).sort();
+//console.log('listAllPackagesSorted',listAllPackagesSorted);
+
+const showDescriptions:(file:Object) => Array<string> = R.compose(
+    R.reject(R.isNil),
+    R.pluck('Description')
+);
+console.log('showDescriptions',showDescriptions(toKeyValuePairs));
+
+/*
+const toPackage = R.compose(
+    R.mergeAll,
+    toKeyValuePairs,
+    toFullArray
+);
+//console.log('toPackage',toPackage(file));
+
+/!*const singlePackageObj:(file:string) => Object = R.compose(
+    //R.mergeAll,
+    R.tap(console.log),
+    R.map(toKeyValuePairs),
+
+
+);*!/
+//console.log('singlePackageObj',singlePackageObj(file));*/
