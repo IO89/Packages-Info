310c317d55f58a4ba3007afa8bc22bb4070bf505
IO89 <osipov.ivan.89@gmail.com>
2019-01-15 10:03:55 +0200

    shows packages name and descriptions

--- src/server/controllers/packages.controller.ts
+++ src/server/controllers/packages.controller.ts
@@ -13,22 +13,14 @@ const moveOneIndexBack:(accumulator:Array<string>,element:string) => Array<strin
         : accumulator.push(element);
     return accumulator
 };
-
-const groupPackages:(accumulator:Array<string>,element:string) => Array<string> = (accumulator, element) => {
-    !element.match(/^Original-Maintainer.*/)
-        ? R.splitWhen(R.prop(accumulator[accumulator.length-1]))
-        : accumulator.push(element);
-   return accumulator
-};
-
 // Transform array to contain fields with full description
 const toFullArray:(file:string)=> Array<string> = R.compose(
     R.reject(n => n== ''),
-    R.reduce(groupPackages,[]),
-    //R.reduce(moveOneIndexBack,[]),
+    //R.reduce(groupPackages,[]),
+    R.reduce(moveOneIndexBack,[]),
     R.split('\n'),
 );
-console.log('fullPackagesObj',toFullArray(file));
+//console.log('fullPackagesObj',toFullArray(file));
 
 const packageArray:Array<string> = toFullArray(file);
 
@@ -37,37 +29,17 @@ const toKeyValuePairs = packageArray.map((element,index)=>{
     let toObjects = {};
     const split = packageArray[index].split(':');
     toObjects[split[0]]=split[1];
-
-    //Create an array of unique depends and cleanup
-    if (toObjects['Depends']) {
-        const removeSpecialCharacters = R.replace(/(\(.*\))/, '');
-        const toDependenciesArray = R.compose(
-            R.uniq,
-            R.map(removeSpecialCharacters),
-            R.split(',')
-        );
-        toObjects['Depends'] = toDependenciesArray(toObjects['Depends'])
-    }
     //console.log('packageArray',toObjects);
     return toObjects
 });
 //console.log('toKeyValuePairs',toKeyValuePairs);
-
-const separateByMaintainer = R.splitWhen(R.prop('Package'));
-
-
-
-
-
-
 
 const showPackagesNames:(file:Object) => Array<string> = R.compose(
     R.reject(R.isNil),
     R.pluck('Package')
 );
 //console.log('showPackagesNames',showPackagesNames(toKeyValuePairs));
-
-const names = showPackagesNames(toKeyValuePairs);
+const packagesNames = showPackagesNames(toKeyValuePairs);
 
 export const listAllPackagesSorted:Array<string> = showPackagesNames(toKeyValuePairs).sort();
 //console.log('listAllPackagesSorted',listAllPackagesSorted);
@@ -79,18 +51,9 @@ const showDescriptions:(file:Object) => Array<string> = R.compose(
 //console.log('showDescriptions',showDescriptions(toKeyValuePairs));
 const descriptions = showDescriptions(toKeyValuePairs);
 
-const showDependecies:(file:Object) => Array<string> = R.compose(
-  R.reject(R.isNil),
-  R.pluck('Depends')
-);
-//console.log('showDependecies',showDependecies(toKeyValuePairs));
-const dependecies = showDependecies(toKeyValuePairs);
+const packagesDescriptions:(packagesNames:Object,descriptions:Object)=>Object =  R.zipObj(packagesNames,descriptions);
+console.log('packagesDescriptions',packagesDescriptions);
 
-const result1 =  R.zipObj(names,descriptions,dependecies);
-//console.log('result',result1);
-
-const searchByName = (name) => R.find(R.propEq(name));
+const searchByName:(name:string)=> Object = (name) => R.find(R.propEq(name));
 //console.log(searchByName('libnfnetlink0')(result1));
 
-const result2 = R.zip(result1,dependecies);
-//console.log('result2',result2);
