5ce15cccb7ad495966d361d55e5f39be1c9e5e87
IO89 <osipov.ivan.89@gmail.com>
2019-01-16 16:28:10 +0200

    new try to display separete packages

--- src/server/controllers/packages.controller.ts
+++ src/server/controllers/packages.controller.ts
@@ -8,41 +8,102 @@ const file:string = fs.readFileSync(statusReal,'utf-8');
 
 // move element if it starts with space to one index back
 const moveOneIndexBack:(accumulator:Array<string>,element:string) => Array<string> = (accumulator, element) => {
-    element.match(/^\s+/g)
+    element.match(/^\s/)
         ? accumulator[accumulator.length-1] += element
         : accumulator.push(element);
     return accumulator
 };
-// Transform array to contain fields with full description
-const toFullArray:(file:string)=> Array<string> = R.compose(
+
+// Transform string to have separate packages by two line breaks
+const splitByPackages:(file:string)=> Array<string> = R.split('\n\n');
+
+//console.log('fullPackagesArray',toFullArray(file));
+
+// Create array separated by line breaks and move values to description
+const splitByKeyValuePairs:(file:string)=> Array<string> = R.compose(
     R.reject(n => n== ''),
     R.reduce(moveOneIndexBack,[]),
-    R.split('\n'),
+    R.split('\n')
 );
-//console.log('fullPackagesObj',toFullArray(file));
 
-const packageArray:Array<string> = toFullArray(file);
+//const packageArray:Array<string> = toFullArray(file);
 
-const separateBy:(accumulator:Array<string>,element:string) => Array<string> = (accumulator, element) => {
-    let count = 0;
-    // @ts-ignore
-    const checkup = element.indexOf(/^Original-Maintainer.*/);
-    console.log('checkup',checkup);
-    accumulator[accumulator.length] = element
-    return accumulator.slice(0,checkup);
-};
-const toPackagesWhole = R.reduce(separateBy,[]);
-console.log(toPackagesWhole(packageArray));
+/*const formatArray = R.compose(
+    R.chain(R.split('\n')),
+    R.slice(0,1)
+);*/
+// Extract key value pairs and convert to object
+const extractKeyPair:(file:string) => Object = R.compose(
+    R.fromPairs(),
+    R.of(),
+    R.slice(1,3),
+    R.match(/(.*):\s(.*)/)
+);
+//console.log('toKeyValuePairList',toKeyValuePairList(file));
+
+// Transform one package to Object with key value pairs
+const toSeparatePackage:(file:string)=> Array<Object> = R.compose(
+    R.mergeAll,
+    R.map(extractKeyPair),
+    splitByKeyValuePairs
+);
+//console.log('toPackageTry',toPackageTry(file));
+
+// Iterate trough file and transform all packages
+const convertPackages:(file:string) => Array<Object> = R.compose(
+    R.map(toSeparatePackage),
+    splitByPackages,
+);
+console.log('toPackagesTryOut',convertPackages(file));
+
+//console.log('formatArray',formatArray(packageArray))
+//const formattedArray =formatArray(packageArray);
+
+//console.log('packageArray',packageArray)
+/*const splitAgain = packageArray.map(element => {
+    let array = [];
+    const split = R.chain(R.split('\n'))(packageArray);
+    return [...array,split]
+});*/
+
+//console.log('splitAgain',splitAgain)
 
 // Separate elements by colon and create key value pairs
-const toKeyValuePairs = packageArray.map((element,index)=>{
+const toKeyValuePairs = /*formattedArray.map((element,index)=>{
     let toObjects = {};
-    const split = packageArray[index].split(':');
+    const split = formattedArray[index].split(':');
     toObjects[split[0]]=split[1];
-    //console.log('packageArray',toObjects);
-    return toObjects
-});
-//console.log('toKeyValuePairs',toKeyValuePairs);
+    return toObjects*/
+R.compose(
+    R.fromPairs,
+    R.of(),
+    R.chain(R.split(':'))
+);
+
+//console.log('toKeyValuePairs',toKeyValuePairs(formattedArray));
+
+/*const parsePackages = R.compose(
+    R.map(toKeyValuePairs),
+    toFullArray
+);*/
+//console.log('parsePackages',parsePackages(file));
+
+// Transform array to have only one package with separate pairs
+const packageKeyPair:(file:Array<string>) => Object = R.compose(
+    R.fromPairs(),
+    R.of(),
+    //R.reduce(splitByNewline,[]),
+    R.slice(0,1),
+
+);
+//console.log('packageKeyPair',packageKeyPair(packageArray));
+
+/*const toPackage = R.compose(
+    //R.mergeAll,
+    R.map(packageKeyPair),
+    toFullArray);*/
+//console.log('toPackage',toPackage(file));
+
 
 const showPackagesNames:(file:Object) => Array<string> = R.compose(
     R.reject(R.isNil),
@@ -66,6 +127,5 @@ const packagesAndDescriptions:(packagesNames:Object,descriptions:Object)=>Object
 
 const searchByName:(name:string)=> any = (name) => R.pluck(name);
 //console.log(searchByName('libkrb5-3')(packagesAndDescriptions));
-
 
 
